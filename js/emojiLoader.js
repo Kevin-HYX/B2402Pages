d03b5d2c8e17689e8f62777a924c080d3a63638b
Kevin-HYX <1814510993@qq.com>
2021-10-28 21:34:22 +0800

    Changes

--- js/emojiLoader.js
+++ js/emojiLoader.js
@@ -3,11 +3,12 @@
  */
 
 count = 4
+let br;
 let container = document.getElementsByClassName("picture_container")[0];
 for (let i = 1, str; i <= count; i++) {
     const emoji = document.createElement("img");
     emoji.setAttribute("src", "https://cdn.jsdelivr.net/gh/Kevin-HYX/B2402Pages/image/emojis/" + Number(i) + ".jpg")
     container.appendChild(emoji)
-    const br = document.createElement("br");
+    br = document.createElement("br");
     container.appendChild(br)
 }
