4800dc450f280dd0fda156c5f37670000da033ae
Kevin-HYX <1814510993@qq.com>
2021-10-31 22:12:06 +0800

    Add Musics And Emoji

--- js/MusicLoader.js
+++ js/MusicLoader.js
@@ -3,7 +3,7 @@ const main_div = document.getElementById("music_time");
 
 $.ajax({
     type: 'GET',
-    url: "http://cdn.jsdelivr.net/gh/Kevin-HYX/B2402Pages/json/new.json",
+    url: "../json/new.json",
     async: true,
     dataType: 'json',
     success(res) {
@@ -11,13 +11,13 @@ $.ajax({
             //播放器所在容器
             const music_div = document.createElement("div");
             music_div.setAttribute("class", "music_player");
-
+            music_div.innerText = res[i].name.toString()
             //播放器
             const audio = document.createElement("audio");
             audio.setAttribute("controls", "controls");
             //链接音频
             const source = document.createElement("source")
-            source.setAttribute("src", "https://cdn.jsdelivr.net/gh/Kevin-HYX/B2402Pages/music/New" + res[i].name.toString())
+            source.setAttribute("src", "https://cdn.jsdelivr.net/gh/Kevin-HYX/B2402Pages/music/New/" + res[i].name.toString())
 
             //添加
             audio.appendChild(source);
