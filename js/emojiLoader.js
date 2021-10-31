47e914b6b75974fcd430cb60300b41d38a170b43
Kevin-HYX <1814510993@qq.com>
2021-10-31 20:25:16 +0800

    Add Musics And Emoji

--- js/emojiLoader.js
+++ js/emojiLoader.js
@@ -2,12 +2,12 @@
  本文件专用于表情包专辑
  */
 
-count = 8
+count = 7
 let br;
 let container = document.getElementsByClassName("picture_container")[0];
 for (let i = 1, str; i <= count; i++) {
     const emoji = document.createElement("img");
-    emoji.setAttribute("src", "https://cdn.jsdelivr.net/gh/Kevin-HYX/B2402Pages/image/emojis/" + Number(i) + ".jpg")
+    emoji.setAttribute("src", "https://cdn.jsdelivr.net/gh/Kevin-HYX/B2402Pages/image/emojis/" + Number(count - i + 1) + ".jpg")
     container.appendChild(emoji)
     br = document.createElement("br");
     container.appendChild(br)
