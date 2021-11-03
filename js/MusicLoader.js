46ab4e1880e63c7c8830d3bb6340108ec000e71c
Kevin-HYX <1814510993@qq.com>
2021-11-03 22:30:05 +0800

    Add Musics And Emoji

--- js/MusicLoader.js
+++ js/MusicLoader.js
@@ -26,31 +26,32 @@ $.ajax({
         }
         const newHR = document.createElement("hr")
         main_div.appendChild(newHR)
+        $.ajax({
+            type: 'GET',
+            url: "https://cdn.jsdelivr.net/gh/Kevin-HYX/B2402Pages/json/old.json?v="+Math.random().toString(),
+            async: true,
+            dataType: 'json',
+            success(res) {
+                for (let i = 0; i < res.length; i++) {
+                    //播放器所在容器
+                    const music_div = document.createElement("div");
+                    music_div.setAttribute("class", "music_player");
+                    music_div.innerText = res[i].name.toString()
+                    //播放器
+                    const audio = document.createElement("audio");
+                    audio.setAttribute("controls", "controls");
+                    //链接音频
+                    const source = document.createElement("source")
+                    source.setAttribute("src", "https://cdn.jsdelivr.net/gh/Kevin-HYX/B2402Pages/music/Old/" + res[i].name.toString())
+
+                    //添加
+                    audio.appendChild(source);
+                    music_div.appendChild(audio)
+                    main_div.appendChild(music_div)
+                }
+            }
+        })
+
     }
 })
 
-$.ajax({
-    type: 'GET',
-    url: "https://cdn.jsdelivr.net/gh/Kevin-HYX/B2402Pages/json/old.json?v="+Math.random().toString(),
-    async: true,
-    dataType: 'json',
-    success(res) {
-        for (let i = 0; i < res.length; i++) {
-            //播放器所在容器
-            const music_div = document.createElement("div");
-            music_div.setAttribute("class", "music_player");
-            music_div.innerText = res[i].name.toString()
-            //播放器
-            const audio = document.createElement("audio");
-            audio.setAttribute("controls", "controls");
-            //链接音频
-            const source = document.createElement("source")
-            source.setAttribute("src", "https://cdn.jsdelivr.net/gh/Kevin-HYX/B2402Pages/music/Old/" + res[i].name.toString())
-
-            //添加
-            audio.appendChild(source);
-            music_div.appendChild(audio)
-            main_div.appendChild(music_div)
-        }
-    }
-})
