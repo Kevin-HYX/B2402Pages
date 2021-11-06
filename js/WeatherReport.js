5452127e25dbd4d39f0fe7858655a93635c140ee
Kevin-HYX <1814510993@qq.com>
2021-11-06 17:00:45 +0800

    Add Musics And Emoji

---
+++ js/WeatherReport.js
@@ -0,0 +1,11 @@
+WIDGET = {
+    "CONFIG": {
+        "layout": "1",
+        "width": "600",
+        "height": "200",
+        "background": "1",
+        "borderRadius": "15",
+        "dataColor": "FFFFFF",
+        "key": "115c4eed29e341eaadfabc5d6b9f5dd4"
+    }
+}
